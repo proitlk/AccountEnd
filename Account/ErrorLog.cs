a67c49fd11e8fb80296a96ba1071d28e3b141b46
Kasun thilanka <thilanka@proitsolutions.lk>
2016-03-03 10:07:10 +0530

    Adding supplier paybel form modifications

---
+++ Account/ErrorLog.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Data;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Security.Cryptography;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+using System.Net.Mail;
+using System.Web.Configuration;
+using System.Net.Configuration;
+using System.Net;
+using System.Text;
+using System.IO;
+using System.Globalization;
+
+namespace Account
+{
+    public class ErrorLog : System.Web.UI.Page
+    {
+        IFormatProvider frmt = new CultureInfo("en-US", true);
+        public void createErrorLog(string strException, string strSource, string strOther)
+        {
+            DateTime strDateTime = DateTime.Now;
+            String Path = Server.MapPath(@"\ErrorLogs");
+            if (!Directory.Exists(Path))
+            {
+                Directory.CreateDirectory(Path);
+            }
+            // StreamWriter sw = new StreamWriter(Server.MapPath("ErrorLog\\ErrorLog-" + dtDateTime.ToString("yyyy-MM-dd hh.mm.ss.tt") + ".txt")); ajTest
+            if (File.Exists(Path + "\\ErrorLog-" + strDateTime.ToString("yyyy-MM-dd") + ".txt"))
+            {
+                StreamWriter sw = new StreamWriter(Path + "\\ErrorLog-" + strDateTime.ToString("yyyy-MM-dd") + ".txt");
+                sw.WriteLine("=============================================================");
+                sw.WriteLine("Time      : " + strDateTime.ToString("yyyy-MM-dd hh:mm:ss tt"));
+                sw.WriteLine("");
+                sw.WriteLine("Exception : " + strException);
+                sw.WriteLine("");
+                sw.WriteLine("Source    : " + strSource);
+                sw.WriteLine("");
+                sw.WriteLine("Other     : " + strOther);
+                sw.WriteLine("");
+                sw.WriteLine("=============================================================");
+                sw.Flush();
+                sw.Close();
+            }
+            else
+            {
+                //Stream s = new FileStream("ErrorLog\\ErrorLog-" + strDateTime.ToString("yyyy-MM-dd") + ".txt", FileMode.Create);
+                StreamWriter sw = new StreamWriter(Path + "\\ErrorLog-" + strDateTime.ToString("yyyy-MM-dd") + ".txt");
+                sw.WriteLine("=============================================================");
+                sw.WriteLine("Time      : " + strDateTime.ToString("yyyy-MM-dd hh:mm:ss tt"));
+                sw.WriteLine("");
+                sw.WriteLine("Exception : " + strException);
+                sw.WriteLine("");
+                sw.WriteLine("Source    : " + strSource);
+                sw.WriteLine("");
+                sw.WriteLine("Other     : " + strOther);
+                sw.WriteLine("");
+                sw.WriteLine("=============================================================");
+                sw.Flush();
+                sw.Close();
+            }
+        }
+    }
+}
---
+++ Account/cls_Connection.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Data;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+using MySql.Data.MySqlClient;
+
+namespace Account
+{
+    public class cls_Connection
+    {
+        private static ErrorLog error = new ErrorLog();
+        private static MySqlConnection connect = null;
+
+        private static MySqlConnection DBConnect()
+        {
+            try
+            {
+                MySqlConnection con = new MySqlConnection();
+                con.ConnectionString = ConfigurationManager.ConnectionStrings["LocalConnection"].ConnectionString;
+                return con;
+            }
+            catch (MySqlException mye)
+            {
+                //error.createErrorLog(mye.Message, mye.Source, "MySQL Error");
+                return null;
+            }
+            catch (Exception e)
+            {
+                //error.createErrorLog(e.Message, e.Source, "Data Connection Error");
+                return null;
+            }
+        }
+        public static void setData(String q)
+        {
+            MySqlCommand com;
+            MySqlDataReader dr;
+            connect = cls_Connection.DBConnect();
+            try
+            {
+                connect.Open();
+                com = new MySqlCommand(q);
+                com.Connection = connect;
+                dr = com.ExecuteReader();
+
+                if (dr.RecordsAffected > 0)
+                {
+                    // MessageBox.Show("Record Successfuly Saved");
+                }
+                closeConnection();
+            }
+            catch (MySqlException mye)
+            {
+                //error.createErrorLog(mye.Message, mye.Source, "MySQL Error");
+                closeConnection();
+            }
+            catch (Exception e)
+            {
+                //error.createErrorLog(e.Message, e.Source, "Data Sending Error");
+                closeConnection();
+            }
+        }
+        public static MySqlDataReader getData(String q)
+        {
+            MySqlCommand com;
+            MySqlDataReader dr;
+            connect = cls_Connection.DBConnect();
+            try
+            {
+                connect.Open();
+                com = new MySqlCommand(q);
+                com.Connection = connect;
+                dr = com.ExecuteReader();
+                return dr;
+            }
+            catch (MySqlException mye)
+            {
+                //error.createErrorLog(mye.Message, mye.Source, "MySQL Error");
+                closeConnection();
+                return null;
+            }
+            catch (Exception e)
+            {
+                //error.createErrorLog(e.Message, e.Source, "Data Downloading Error");
+                closeConnection();
+                return null;
+            }
+        }
+
+        public static DataSet getDataSet(String q)
+        {
+            MySqlCommand com;
+            MySqlDataAdapter da;
+            DataSet ds;
+            connect = cls_Connection.DBConnect();
+            try
+            {
+                connect.Open();
+                com = new MySqlCommand(q);
+                com.Connection = connect;
+                da = new MySqlDataAdapter(com);
+                ds = new DataSet();
+                da.Fill(ds);
+                return ds;
+            }
+            catch (MySqlException mye)
+            {
+                //error.createErrorLog(mye.Message, mye.Source, "MySQL Error");
+                closeConnection();
+                return null;
+            }
+            catch (Exception e)
+            {
+                //error.createErrorLog(e.Message, e.Source, "Data Downloading Error");
+                closeConnection();
+                return null;
+            }
+        }
+
+        public static void closeConnection()
+        {
+            if (connect.State == System.Data.ConnectionState.Open)
+            {
+                connect.Close();
+                connect.Dispose();
+            }
+        }
+    }
+}
